2dc0cf828323015ecf13025cd897b00a5c8c83d9
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-27 00:19:58 +1100

    Added cards to response

--- app.js
+++ app.js
@@ -48,6 +48,80 @@ var basicQnAMakerDialog = new builder_cognitiveservices.QnAMakerDialog({
                 qnaThreshold: 0.3}
 );
 
+// override
+basicQnAMakerDialog.respondFromQnAMakerResult = function(session, qnaMakerResult){
+    // Save the question
+    var question = session.message.text;
+    session.conversationData.userQuestion = question;
+
+    // boolean to check if the result is formatted for a card
+    var isCardFormat = qnaMakerResult.answers[0].answer.includes(';');
+
+    if(!isCardFormat){
+        // Not semi colon delimited, send a normal text response 
+        session.send(qnaMakerResult.answers[0].answer);
+    }else if(qnaMakerResult.answers && qnaMakerResult.score >= 0.5){
+        
+        var qnaAnswer = qnaMakerResult.answers[0].answer;
+        
+                var qnaAnswerData = qnaAnswer.split(';');
+                var title = qnaAnswerData[0];
+                var description = qnaAnswerData[1];
+                var url = qnaAnswerData[2];
+                var imageURL = qnaAnswerData[3];
+        
+                var msg = new builder.Message(session)
+                msg.attachments([
+                    new builder.HeroCard(session)
+                    .title(title)
+                    .subtitle(description)
+                    .images([builder.CardImage.create(session, imageURL)])
+                    .buttons([
+                        builder.CardAction.openUrl(session, url, "Learn More")
+                    ])
+                ]);
+        }
+    session.send(msg).endDialog();
+}
+
+basicQnAMakerDialog.defaultWaitNextMessage = function(session, qnaMakerResult){
+    // saves the user's question
+    session.conversationData.userQuestion = session.message.text; 
+    
+    if(!qnaMakerResult.answers){
+        let msg = new builder.Message(session)
+        .addAttachment({
+            contentType: "application/vnd.microsoft.card.adaptive",
+            content: {
+                type: "AdaptiveCard",
+                body: [
+                    {
+                        "type": "TextBlock",
+                        "text": `${session.conversationData.userQuestion}`,
+                        "size": "large",
+                        "weight": "bolder",
+                        "color": "accent",
+                        "wrap": true
+                    },
+                    {
+                        "type": "TextBlock",
+                        "text": `Sorry, no answer found in QnA service`,
+                        "size": "large",
+                        "weight": "regular",
+                        "color": "dark",
+                        "wrap": true
+                    }
+                ]
+            }
+        });
+        session.send(msg);
+    }
+    session.endDialog();
+}
+
+bot.dialog('/', basicQnAMakerDialog);
+
+/*
 bot.dialog('welcome', [
     function (session) {
         // Send a greeting and show help.
@@ -70,3 +144,4 @@ bot.dialog('/',
             session.replaceDialog('basicQnAMakerDialog');
     }
 ]);
+*/
