d5ed54857a959da674584060fcd5fb627368f242
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-27 00:08:02 +1100

    fixed syntex error with undefined /

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ bot.dialog('welcome', [
 
 bot.dialog('basicQnAMakerDialog', basicQnAMakerDialog);
 
-bot.dialog('/', basicQnAMakerDialog
+bot.dialog('/',
 [
     function (session){
         var qnaKnowledgebaseId = process.env.QnAKnowledgebaseId;
