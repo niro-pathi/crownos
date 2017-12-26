0d94a83290aa8a1cc93c0ae4a0314cc090e29131
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-27 10:20:04 +1100

    Added more friendly messsages

--- src/strings.js
+++ src/strings.js
@@ -2,16 +2,16 @@ module.exports = {
     QUERY_RESULT_COUNTS: 3,
 
     INTRO_TITLE: 'I am Crownos.',
-    INTRO_TEXT: 'I am here to help you find answer(s) of Crown Connect Support questions. You can also check the FAQ on Confluance.',
+    INTRO_TEXT: 'I am here to help you find answer(s) of Crown Connect Support questions.\nYou can also check the FAQ on Confluance.',
     INTRO_BUTTON_TITLE: 'Help',
     INTRO_BUTTON_VALUE: 'Want to know more?',
 
     HELP_BUTTON_TITLE_ASK: 'Ask a question',
-    HELP_CARD_TEXT: 'You can ask me anything about Crown Connect.Select a command to learn how to use it.',
+    HELP_CARD_TEXT: 'You can ask me anything about Crown Connect. \n Select a command to learn how to use it.',
     HELP_CHECK_THE_FAQ: 'Check the FAQ Confluance page',
     HELP_ASK_A_QUESTION: 'You can ask me anything about Crown Connect.Please start the question with **what is, How, Does, Do, Is there, Where**, and end the question with a question mark. For example: What is Crown Connect?',
     
-    NO_MATCH_FROM_SERVICE: 'Sorry, this may be beyond my abilities at the moment.Try asking for **help**',
+    NO_MATCH_FROM_SERVICE: 'Sorry, this may be beyond my abilities at the moment. \n Try asking for **help**',
     NO_ANSWER_FOUND: 'No good match found in the KB',
     PROPOSE_QUESTION_CARD_TITLE: 'Are these questions you want to ask?',
     FAQ_URL: 'https://crownmelbourne.atlassian.net/wiki/spaces/PLUP/pages/45711648/Loyalty+Post+Release+FAQ'
