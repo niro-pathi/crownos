96d433903b6809bad7fd9eb6f547dc0e838793a7
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-27 21:12:05 +1100

    change luis url

--- src/dialogs/checkPlatform.js
+++ src/dialogs/checkPlatform.js
@@ -8,15 +8,15 @@ var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
 var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
- 
-console.log('10 Inside checkPlatform' + LuisModelUrl);
+const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v2.0/apps' + luisAppId + '&subscription-key=' + luisAPIKey;
+
+console.log('10 Inside checkPlatform ' + LuisModelUrl);
 
  // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 bot.recognizer(recognizer);
 
-console.log('020 Inside checkPlatform');
+console.log('020 Inside checkPlatform ');
 
 bot.dialog('VIK_Status', [
     function (session,args,next) {
