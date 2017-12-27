a890ae3acce771d1e50527e1bc6c198f2d5d6562
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-27 21:05:24 +1100

    Added more debug logs

--- src/dialogs/checkPlatform.js
+++ src/dialogs/checkPlatform.js
@@ -1,17 +1,22 @@
 const _ = require('lodash');
 var fs = require('fs');
 
+console.log('00 Inside checkPlatform');
+
 // Make sure you add code to validate these fields
 var luisAppId = process.env.LuisAppId;
 var luisAPIKey = process.env.LuisAPIKey;
 var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
 
 const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' + luisAppId + '&subscription-key=' + luisAPIKey;
+ 
+console.log('10 Inside checkPlatform' + LuisModelUrl);
+
  // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 bot.recognizer(recognizer);
 
-console.log('Inside checkPlatform');
+console.log('020 Inside checkPlatform');
 
 bot.dialog('VIK_Status', [
     function (session,args,next) {
