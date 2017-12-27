a422204d59b17e4e5bfbfb1f6adc38c212a0b67e
Niroshana Pathirana <niroshana.pathirana@outlook.com>
2017-12-27 15:38:58 +1100

    Added more to the help card

--- src/strings.js
+++ src/strings.js
@@ -7,11 +7,16 @@ module.exports = {
     INTRO_BUTTON_VALUE: 'Want to know more?',
 
     HELP_BUTTON_TITLE_ASK: 'Ask a question',
-    HELP_CARD_TEXT: 'You can ask me anything about Crown Connect. \n Select a command to learn how to use it.',
+    HELP_CARD_TEXT: 'You can ask me anything about Crown Connect.\nSelect a command to learn how to use it.',
     HELP_CHECK_THE_FAQ: 'Check the FAQ Confluance page',
-    HELP_ASK_A_QUESTION: 'You can ask me anything about Crown Connect.Please start the question with **what is, How, Does, Do, Is there, Where**, and end the question with a question mark. For example: What is Crown Connect?',
+    HELP_ASK_A_QUESTION: 'You can ask me anything about Crown Connect.\nPlease start the question with **what is, How, Does, Do, Is there, Where**, and end the question with a question mark. For example: What is Crown Connect?',
     
-    NO_MATCH_FROM_SERVICE: 'Sorry, this may be beyond my abilities at the moment. \n Try asking for **help**',
+    FIX_BUTTON_TITLE_ASK: 'Make a Data Fix',
+    FIX_CARD_TEXT: 'You can ask me anything about Crown Connect data fixes.\nSelect a command to learn how to use it.',
+    FIX_CHECK_THE_FAQ: 'Check the Data Fix Confluance page',
+    FIX_ASK_A_QUESTION: 'You can ask me anything about Crown Connect data fix.\nPlease start the question with **what is, How, Does, Do, Is there, Where**, and end the question with a question mark. For example: What is Crown Connect?',
+
+    NO_MATCH_FROM_SERVICE: 'Sorry, this may be beyond my abilities at the moment. \nTry asking for **help**',
     NO_ANSWER_FOUND: 'No good match found in the KB',
     PROPOSE_QUESTION_CARD_TITLE: 'Are these questions you want to ask?',
     FAQ_URL: 'https://crownmelbourne.atlassian.net/wiki/spaces/PLUP/pages/45711648/Loyalty+Post+Release+FAQ'
